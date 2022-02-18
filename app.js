ec891f52637830486757cc37720a4886965250be
Antony Wyatt <toileok1@gmail.com>
2022-02-17 21:29:44 -0500

    up

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const cors = require('cors')
+const nodemailer = require('nodemailer')
+
+const bodyParser = require('body-parser')
+
+const PORT = process.env.PORT || 9000;
+
+const app = express();
+
+app.use(cors())
+app.use(bodyParser.json())
+
+//Routes 
+app.get('/', (req, res) => {
+    res.send('Ir a url🍕')
+})
+
+app.post('/send-mail', async (req, res) => {
+
+    const { 
+      from, // AQUÍ < >
+      mail, 
+      subject, 
+      html,
+      _host,
+      _port,
+      _user,
+      _pass,
+    } = req.body
+
+    const transporter = nodemailer.createTransport({
+        host: _host,
+        port: _port,
+        secure: false,
+        auth: {
+            user: _user,
+            pass: _pass
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    })
+
+    const info = await transporter.sendMail({
+        from: `'${from}' <${_user}>`,
+        to: mail,
+        subject: subject,
+        html:  `${html}`
+    })
+
+    res.send(info)
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+})
